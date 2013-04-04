bae39ae98393dc34267839b36a2d7c8466fa7c87
Michael Hallberg <info@yard.se>
2013-04-04 16:36:36 +0200

    bundlat lite

--- App_Code/BundleConfig.cs
+++ App_Code/BundleConfig.cs
@@ -27,6 +27,10 @@ namespace Eaztimate
                 "~/Scripts/WebForms/MsAjax/MicrosoftAjaxTimer.js",
                 "~/Scripts/WebForms/MsAjax/MicrosoftAjaxWebForms.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/CustomJS").Include(
+                    "~/Scripts/jquery-{version}.js",
+                    "~/Scripts/script.js"));
+
             // Use the Development version of Modernizr to develop with and learn from. Then, when you’re
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
