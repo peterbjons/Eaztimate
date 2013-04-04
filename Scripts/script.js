98c93f83f04cf3170ec3b56876851b3785aab790
Michael Hallberg <info@yard.se>
2013-04-04 16:42:15 +0200

    commie 2

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,1 +1,12 @@
-﻿
+﻿function expandList(handle, content) {
+    $(handle).click(function () {
+        var speed = 200;
+        var hide = ($(this).closest('li').find(content).css('display') != 'none');
+        $(content).hide(speed);
+        if (!hide) {
+            $(this).closest('li').find(content).show(speed);
+        } else {
+
+        }
+    });
+}
