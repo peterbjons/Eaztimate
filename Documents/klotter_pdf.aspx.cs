572eee3a90470ccf3632333301b79c8c94b2e816
Mattias Hjort <mattias@yard.se>
2013-06-19 16:37:45 +0200

    pushy!!!

--- Documents/klotter_pdf.aspx.cs
+++ Documents/klotter_pdf.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,10 @@ public partial class Documents_klotter_pdf : System.Web.UI.Page
     {
         int klotterid = 0;
         int.TryParse(Request.QueryString["id"] ?? "5", out klotterid);
-
+        using (SqlDataReader reader = Eaztimate.SQL.ExecuteQuery("SELECT a.*,(SELECT TOP 1 timestamp FROM jour_log WHERE jourid=@1 ORDER BY timestamp ASC) timestamp,(SELECT TOP 1 x.image FROM jourimage x WHERE x.jourid=@1 order by image) jourimage FROM jour a WHERE jourid=@1;SELECT title FROM jour_room WHERE jourid=@1", jourid)) {
+            if (reader.Read()) {
+            }
+        }
 
     }
 }
